7266d7223f14db4bbac8710b54ceadad55c08bf8
Yue Lin <yue.lin@door3.com>
2020-07-27 23:29:46 -0400

    collideee

---
+++ Frames Per Second/Assets/Nucleon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
