d26c63460d9b56985286b4182faad41db8450eee
Yue Lin <yue.lin@door3.com>
2020-07-28 00:59:28 -0400

    label doesn't update

---
+++ Frames Per Second/Assets/FPSCounter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int FPS { get; private set; }
+
+    void Upate()
+    {
+        /* use unscaledDeltaTime because the Time.deltaTime is not the actual time it took to process the last frame,
+         * it is influenced by the current time scale.
+         */
+        FPS = (int)(1f / Time.unscaledDeltaTime);
+    }
+}
